021b87b6500f17ca937dc052588f2a59408a30cc
emer2006 <126901936+emer2006@users.noreply.github.com>
2023-03-11 00:42:47 -0500

    Update fun-reto.js

--- comandos/fun-reto.js
+++ comandos/fun-reto.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-conn.reply(m.chat,`*┌────「 𝚁𝙴𝚃𝙾 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「  𝙳𝙾𝚁𝚁∆𝚃  」─*`, m)
+conn.reply(m.chat,`*┌────「 𝚁𝙴𝚃𝙾 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「  𝑪𝒉𝒂𝒓𝒍𝒊𝒆𝑩𝒐𝒕  」─*`, m)
 }
 handler.help = ['reto']
 handler.tags = ['fun']
